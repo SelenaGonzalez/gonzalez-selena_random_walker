55478480b876e417f9d8f5fec5ad2149405396f2
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-10-20 23:07:51 -0500

    Random Walker

--- sketch.js
+++ sketch.js
@@ -1,36 +1,32 @@
-let W1;
-let W2;
-let W3;
-let W4;
+let w1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  W1 = new Walker(200, 200);
-  W2 = new Walker(200, 200);
-  W3 = new Walker(200, 200);
-  W4 = new Walker(200, 200);
+  w1 = new Walker(200, 200);
 }
 
 function draw() {
-  W1.update();
-  W1.display();
-  W2.update();
-  W2.display();
-  W3.update();
-  W3.display();
-  W4.update();
-  W4.display();
+  w1.update();
+  w1.display();
 }
 
 class Walker {
   constructor(_x, _y) {
     this.posx = _x;
     this.posy = _y;
-    this.vel = random(1, 10);
-    this.color = color(255, random(0, 200), random(0, 200));
-    this.esCirculo = random() > 0.5 ? true : false;
-  }
+    this.vel = random(1, 70);
+    this.color = color(random(0, 200), random(0, 255), random(0, 200));
 
+    this.esCirculo = random() > 0.5 ? true : false;
+
+    // this.esCirculo;
+    // this.r = random();
+    // if (this.r > 0.5) {
+    // 	this.esCirculo = true;
+    // } else {
+    // 	this.esCirculo = false;
+    // }
+  }
   update() {
     this.posx += random(-this.vel, this.vel);
     this.posy += random(-this.vel, this.vel);
@@ -38,11 +34,12 @@ class Walker {
 
   display() {
     fill(this.color);
-    noStroke();
-    if (this.esCriculo) {
-      circle(this.posx, this.posy, 10);
+    // noStroke();
+    stroke(255);
+    if (this.esCirculo) {
+      circle(this.posx, this.posy, random(0, 50));
     } else {
-      rect(this.posx, this.posy, 10, 10);
+      rect(this.posx, this.posy, 40, random(0, 50));
     }
   }
 }
